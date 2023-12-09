5edb6f3c4cc233d2665abbec0bdae3243e1ed9cc
dcabezasdc <145324063+dcabezasdc@users.noreply.github.com>
2023-12-09 21:57:11 +0100

    Update Boletin04_1.java

--- Boletin04/Boletin04_1/Boletin04_1.java
+++ Boletin04/Boletin04_1/Boletin04_1.java
@@ -1,10 +1,22 @@
 package boletin04_1;
-//Crea unha clase chamada Libro que conteña os seguintes atributos:
+//1- Crea unha clase chamada Libro que conteña os seguintes atributos:
 // a. String titulo;
 // b. String autor;
 // c. int ano;
 // d. short numPaginas;
 // e. float valoracion;
+
+//2- A clase debe de ter os seguintes construtores:
+//    ● Construtor por defecto. 
+//    ● Construtor que permita inicializar o título , autor , ano e número de páxinas.
+
+//3- Establecer os métodos de acceso para todos os atributos. 
+
+//4-Metodo amosar que non devolve nada e visualiza todas as característica dun libro .
+
+//5- Crear unha clase Principal co método main.
+//Crear un libro con cada construtor e mostrar por consola a súa información. Modificar os datos do libro 
+//creado co construtor por defecto
 
 public class Boletin04_1 {
 
