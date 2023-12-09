ccb380c6cdf8ccdb4d5b5a7c8a717f1475dc00e1
dcabezasdc <145324063+dcabezasdc@users.noreply.github.com>
2023-12-09 21:52:08 +0100

    Update Boletin03_5.java

--- Boletin03/Boletin03_5/Boletin03_5.java
+++ Boletin03/Boletin03_5/Boletin03_5.java
@@ -1,4 +1,14 @@
 package boletin03_5;
+//Fai o algoritmo e programa  que calcule o soldo bruto e líquido, a percibir, por unha persoa.
+//Sabendo que o soldo total inclue os seguintes conceptos :
+
+//	•	Soldo fixo. 
+//	•	Comisión: 5% sobre importe total de vendas
+//	•	Quilometraxe: 2 € por km
+//	•	Dietas: 30 € por día de desprazamento .
+//Para calcular o soldo líquido debemos descontarlle ao soldo bruto :
+//	•	I.R.P.F. = 18 % do soldo total .
+//	•	Retención a seguridade social: 36 € .
 
 import java.util.Scanner;
 
