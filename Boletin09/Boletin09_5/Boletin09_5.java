63d2d1e7f8bb7b9cd1f5c82a51da6d4ea466f09a
dcabezasdc <145324063+dcabezasdc@users.noreply.github.com>
2023-12-10 00:27:41 +0100

    Update Boletin09_5.java

--- Boletin09/Boletin09_5/Boletin09_5.java
+++ Boletin09/Boletin09_5/Boletin09_5.java
@@ -1,9 +1,9 @@
+package boletin09_5;
 /*
 Codifica un programa que  Lea o soldo de cada un dos traballadores dunha empresa e obteña o número deles que ganan entre 1000 e 1750 €, 
 ámbolos dous incluidos e, a porcentaje de traballadores que ganan menos de 1000 €.Tendo en conta que non se coñece con antelación o numero 
 de traballadores da empresa e non se admiten os soldos negativos.( utiliza como condidión de fin un soldo 0 ). 
 */
-package boletin09_5;
 
 public class Boletin09_5 {
 
