35ddba1235454395de47a1c7dfc061ba79e2ca7b
dcabezasdc <145324063+dcabezasdc@users.noreply.github.com>
2023-12-10 00:08:50 +0100

    Update Boletin05_1.java

--- Boletin05/Boletin05_1/Boletin05_1.java
+++ Boletin05/Boletin05_1/Boletin05_1.java
@@ -1,4 +1,29 @@
 package boletin05_1;
+/*
+Implementa unha clase consumo, que forma parte da centralita electrónica dun coche e ten as seguintes características :
+Atributos :
+km   kilómetros percorridos polo coche
+litros  Litros de combustible consumidos
+vMedvelocidade media
+pGas  Prezo da gasolina
+Metodos :
+Dous constructores , un con parámetros e outro sen eles, que inicializan os  valores dos atributos
+getTempo  Indica o tempo empregado en realizar a viaxe
+consumoMedio consumo medio do vehículo ( en litros cada 100 km )
+consumoEuros consumo medio( en € cada 100 km )
+setKms  modifica o valor dos km
+setLitros “             “  “       “    litros                      
+setvMed   “            “              vMed
+ setPGas  “”             “             pGas.
+
+Na clase principal :
+	•	Crea un obxecto, de tipo consume, utilizando o constructor sen parámetros
+	•	Dalle a litros o valor 50 e a prezo da gasolina 1.57
+	•	Crea un obxecto, tipo consumo, utilizando o contructor con todos os parámetros
+	•	Visualiza, a  través do 2º obxecto, o consumo medio
+	•	Varia o valor dos litros consumidos do 2º obxecto.
+	•	Visualiza a velocidade media do 2º obxecto.
+*/
 
 import java.util.Scanner;
 
