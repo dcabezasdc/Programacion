04045cce71ff6198b740a7671924bfcc044f10bf
dcabezasdc <145324063+dcabezasdc@users.noreply.github.com>
2023-12-09 21:42:16 +0100

    Update Boletin03_1.java

--- Boletin03/Boletin03_1/Boletin03_1.java
+++ Boletin03/Boletin03_1/Boletin03_1.java
@@ -1,7 +1,7 @@
 package boletin03_1;
 //Realiza o ordinograma  correspondente a un programa que saque por pantalla a 
-// porcentaxe descontada nunha compra. Introducindo, por teclado, o prezo da tarifa
-// e o prezo pagado. Fai un proxecto, de nome boletin3_1, para executar o programa  
+//porcentaxe descontada nunha compra. Introducindo, por teclado, o prezo da tarifa
+//e o prezo pagado. Fai un proxecto, de nome boletin3_1, para executar o programa  
 
 import java.util.Scanner;
 
