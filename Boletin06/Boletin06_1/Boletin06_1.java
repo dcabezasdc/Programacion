2d7a085c7043621fa1ed6db8ea3993aeab9c99ac
dcabezasdc <145324063+dcabezasdc@users.noreply.github.com>
2023-12-10 00:12:09 +0100

    Update Boletin06_1.java

--- Boletin06/Boletin06_1/Boletin06_1.java
+++ Boletin06/Boletin06_1/Boletin06_1.java
@@ -1,4 +1,24 @@
 package boletin06_1;
+/*
+Temos a seguinte clase Coche:
+
+public class Coche {
+     private int velocidade ;
+
+    public Coche(){
+    velocidade =0 ;
+}
+
+Engade a clase Coche os seguintes métodos :
+
+public int getVelocidade() . Este método devolve a velocidade actual
+
+public void acelerar (int valor) .Este método incrementa a velocidade na cantidade valor
+
+public void frenar (int menos).Este método diminue a velocidade na cantidade menos.
+
+Crea a clase Boletin 6_1 para comprobar que o programa se executa ben .dandolle os valores que precises.
+*/
 
 import java.util.Scanner;
 
