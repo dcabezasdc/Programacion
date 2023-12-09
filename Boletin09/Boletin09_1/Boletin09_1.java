a55c3c331399a1d5223df970a3add1b50dc630f7
dcabezasdc <145324063+dcabezasdc@users.noreply.github.com>
2023-12-09 20:59:24 +0100

    Add files via upload

---
+++ Boletin09/Boletin09_1/Boletin09_1.java
@@ -0,0 +1,13 @@
+/*
+Calcula a cantidade de números negativos, positivos e, ceros  que hai nun grupo de 10 numeros enteiros.
+*/
+package boletin09_1;
+
+public class Boletin09_1 {
+
+    public static void main(String[] args) {
+        Numeros obx =new Numeros();
+        
+        obx.contarNumeros();
+    }    
+}
