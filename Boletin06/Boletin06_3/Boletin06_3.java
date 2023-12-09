b172f5c024393e2c24de37011528f589e9aefebf
dcabezasdc <145324063+dcabezasdc@users.noreply.github.com>
2023-12-10 00:15:53 +0100

    Update Boletin06_3.java

--- Boletin06/Boletin06_3/Boletin06_3.java
+++ Boletin06/Boletin06_3/Boletin06_3.java
@@ -1,4 +1,17 @@
 package boletin06_3;
+/*
+Implementa unha aplicación que nos permita traballar con circulos e circunferencias .Para eso crea a clase Circulo cas seguintes características :
+
+atributos : radio ( tipo double )
+Pi ( constante de tipo double e valor 3.14 )
+
+métodos : *   constructores, sen e con parámetros
+                 *  métodos de acceso
+                 *   calcularArea : Non recibe nada e devolve o valor da área . Para calcular radio ^ 2 utiliza a clase Math e o método pow (….)
+                 *  calcularLonxitude : Non recibe nada e devolve o valor da lonxitude da circunferencias
+   
+     Crea unha clase principal para comprobar que todo funciona ben
+*/
 
 public class Boletin06_3 {
 
