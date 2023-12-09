7e8164b59ab70a800c38911c78b1217cfcc67d47
dcabezasdc <145324063+dcabezasdc@users.noreply.github.com>
2023-12-10 00:33:48 +0100

    Update Boletin11_2.java

--- Boletin11/Boletin11_2/Boletin11_2.java
+++ Boletin11/Boletin11_2/Boletin11_2.java
@@ -1,7 +1,11 @@
 /*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
+Programa o exercicio  anterior para xogar contra o ordenador . Para eso, o ordenador xenera un número aleatorio, 
+entre 1 e 50 ,.Este programa dará as seguintes pistas sobre lonxe que esté de adivinar o número :
+Se a distancia entre o número a adivinar e o tecleado é maior de 20  “ moi lonxe “
+Entre 10 e 20 , ambos incluidos,  lonxe
+Maior de 10 e menor de 5  preto
+Menor ou igual de 5  moi preto .    
+*/
 package boletin11_2;
 
 import javax.swing.JOptionPane;
