98e55cac9e2f3d8f679f5bb0c5f431768b9b765e
dcabezasdc <145324063+dcabezasdc@users.noreply.github.com>
2023-12-09 21:46:42 +0100

    Update Boletin03_4.java

--- Boletin03/Boletin03_4/Boletin03_4.java
+++ Boletin03/Boletin03_4/Boletin03_4.java
@@ -1,4 +1,6 @@
 package boletin03_4;
+//Realiza un algoritmo e codifica o programa correspondente  que lea, dende o teclado, unha cantidade enteira
+//en euros e amose , por pantalla, o seu desglose en billetes de 100, 20, 5 e moedas de 1 €.
 
 import java.util.Scanner;
 
