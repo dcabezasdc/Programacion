947ff97611ecdc57f2789918157e3cbd84f168fa
dcabezasdc <145324063+dcabezasdc@users.noreply.github.com>
2023-12-09 18:13:55 +0100

    Add files via upload

---
+++ Boletin02/Boletin02_2/Boletin02_2.java
@@ -0,0 +1,13 @@
+package boletin02_2;
+//Realiza un programa que permita calcular o área dun cadrado de 3m de lado
+
+public class Boletin02_2 {
+
+   
+    public static void main(String[] args) {
+       float lado = 3;
+        float area = (float) Math.pow(lado, 2);
+        
+        System.out.println("El área del cuadrado es: "+area);        
+    }    
+}
