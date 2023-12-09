d3a57805f62dca5d31fca6cfbe8a762a36fa972e
dcabezasdc <145324063+dcabezasdc@users.noreply.github.com>
2023-12-10 00:26:47 +0100

    Update Boletin09_3.java

--- Boletin09/Boletin09_3/Boletin09_3.java
+++ Boletin09/Boletin09_3/Boletin09_3.java
@@ -1,7 +1,7 @@
+package boletin09_3;
 /*
 Implementa un programa que calcule o área dun rectángulo cuxa base e altura pides por teclado. Asegurate que estos valores sexan positivos, para eso valida os datos.
 */
-package boletin09_3;
 
 public class Boletin09_3 {
 
