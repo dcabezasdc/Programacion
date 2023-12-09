00c8cdc7f60dd2ab71947a88f16dac7c2de7b1f9
dcabezasdc <145324063+dcabezasdc@users.noreply.github.com>
2023-12-09 18:03:26 +0100

    Add files via upload

---
+++ Boletin02/Boletin02_1/Boletin02_1.java
@@ -0,0 +1,13 @@
+package boletin02_1;
+//Deseña un algoritmo que calcule o área dun triángulo. A  saida faise  por pantalla 
+//( para codificar este programa inicializa a base ao valor 4, e a altura ao valor 3 ) 
+
+public class Boletin02_1 {
+
+   
+    public static void main(String[] args) {
+       byte base = 4, altura = 3, area;
+       area = (byte) ((base * altura) / 2);
+       System.out.println("El área del triángulo es: "+area);
+    }    
+}
