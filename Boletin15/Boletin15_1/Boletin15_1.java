6b72dce34827901469fb3def73dcc4182a6ff34d
dcabezasdc <145324063+dcabezasdc@users.noreply.github.com>
2023-12-10 14:00:56 +0100

    Update Boletin15_1.java

--- Boletin15/Boletin15_1/Boletin15_1.java
+++ Boletin15/Boletin15_1/Boletin15_1.java
@@ -1,1 +1,17 @@
+package boletin15_1;
 
+
+public class Boletin15_1 {
+
+    public static void main(String[] args) {
+        
+        Futbolista fut=new Futbolista(5,"Central",9,"Pepe","Fernandez",35);
+        
+        SeleccionFutbol ent=new Entrenador("CC",8,"Pep","Guardiola",50);
+        
+        
+        fut.setEdade(25);
+        System.out.println(fut);
+        System.out.println(ent);
+    }
+}
