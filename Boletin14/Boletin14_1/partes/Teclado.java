dc72e43863cc4caf4544cb88a2fc98ec75d740ef
dcabezasdc <145324063+dcabezasdc@users.noreply.github.com>
2023-12-10 13:57:17 +0100

    Update Teclado.java

--- Boletin14/Boletin14_1/partes/Teclado.java
+++ Boletin14/Boletin14_1/partes/Teclado.java
@@ -1,1 +1,18 @@
+package partes;
 
+public class Teclado {
+    
+    private String marca;
+
+    public Teclado() {
+    }
+
+    public Teclado(String marca) {
+        this.marca = marca;
+    }
+
+    @Override
+    public String toString() {
+        return "\nMarca do teclado= " + marca;
+    }
+}
