ee249371e99819b540d523fdca90050a8969321e
dcabezasdc <145324063+dcabezasdc@users.noreply.github.com>
2023-12-10 13:55:32 +0100

    Update Monitor.java

--- Boletin14/Boletin14_1/partes/Monitor.java
+++ Boletin14/Boletin14_1/partes/Monitor.java
@@ -1,1 +1,18 @@
+package partes;
 
+public class Monitor {
+    
+    private float pulgadas;
+
+    public Monitor() {
+    }
+
+    public Monitor(float pulgadas) {
+        this.pulgadas = pulgadas;
+    }
+
+    @Override
+    public String toString() {
+        return "\nPulgadas do monitor= "  + pulgadas;
+    }
+}
