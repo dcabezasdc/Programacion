d929c84a1d068c991e845fc69800d912921f5635
dcabezasdc <145324063+dcabezasdc@users.noreply.github.com>
2023-12-10 13:56:33 +0100

    Update Procesador.java

--- Boletin14/Boletin14_1/partes/Procesador.java
+++ Boletin14/Boletin14_1/partes/Procesador.java
@@ -1,1 +1,18 @@
+package partes;
 
+public class Procesador {
+    
+    private float velocidade;
+
+    public Procesador() {
+    }
+
+    public Procesador(float velocidade) {
+        this.velocidade = velocidade;
+    }
+
+    @Override
+    public String toString() {
+        return "\nVelocidade do procesador= " + velocidade;
+    }
+}
