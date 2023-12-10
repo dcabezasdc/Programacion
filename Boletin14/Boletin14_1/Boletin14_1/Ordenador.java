725e662d708e0ca3fe79685d5e03b8a7b6f23b04
dcabezasdc <145324063+dcabezasdc@users.noreply.github.com>
2023-12-10 13:50:21 +0100

    Update Ordenador.java

--- Boletin14/Boletin14_1/Boletin14_1/Ordenador.java
+++ Boletin14/Boletin14_1/Boletin14_1/Ordenador.java
@@ -1,1 +1,26 @@
+package boletin14_01;
 
+import partes.*;
+
+public class Ordenador {
+    
+    private Monitor mo;
+    private Teclado te;
+    private Procesador pro;
+    private float precio;
+
+    public Ordenador() {
+    }
+    
+    public Ordenador(Monitor mo, Teclado te, Procesador pro, float precio) {
+        this.mo = mo;
+        this.te = te;
+        this.pro = pro;
+        this.precio = precio;
+    }
+
+    @Override
+    public String toString() {
+        return "Ordenador: " + mo  + te + pro +"\n"+ "Precio= "+precio ;
+    }
+}
