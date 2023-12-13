1aa222bdc4e04551a593a72271f97c51074acd2d
dcabezasdc <145324063+dcabezasdc@users.noreply.github.com>
2023-12-13 10:12:05 +0100

    Update BoletinExtra05_1.java

--- Boletines Extra/BoletinExtra05/BoletinExtra05_1/BoletinExtra05_1.java
+++ Boletines Extra/BoletinExtra05/BoletinExtra05_1/BoletinExtra05_1.java
@@ -1,1 +1,45 @@
+package extra_boletin5;
 
+import javax.swing.JOptionPane;
+
+
+public class Extra_boletin5 {
+
+
+    public static void main(String[] args) {
+       
+       Venta obx=new Venta();
+        /*
+       switch (metodopago = JOptionPane.showInputDialog("Indique el metodo de pago: Efectivo||Tarxeta de crédito||Vale de regalo")){
+           
+           case "Efectivo": 
+               obx.mostrar();
+               break;
+           case "Tarxeta de crédito":
+               obx.mostrar();    
+               break;
+           case "Vale de regalo":
+               obx.mostrar();
+               break;
+           default: 
+               System.out.println("Metodo no valido, pruebe con otro metodo");                  
+       } */
+        
+        switch (obx.setMetodoPago(JOptionPane.showInputDialog("Indique el metodo de pago: Efectivo||Tarxeta de crédito||Vale de regalo"))){
+           
+           case "Efectivo":
+               obx.setCantidad(Long.parseLong(JOptionPane.showInputDialog("Indique a cantidade de impresoras")));
+               obx.mostrar();
+               break;
+           case "Tarxeta de crédito":
+               obx.mostrar();    
+               break;
+           case "Vale de regalo":
+               obx.mostrar();
+               break;
+           default: 
+               System.out.println("Metodo no valido, pruebe con otro metodo");                  
+       }
+    }
+    
+}
