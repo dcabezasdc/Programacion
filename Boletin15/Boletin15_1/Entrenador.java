f68fa82130efad6aa85e1597df361274a8bb0700
dcabezasdc <145324063+dcabezasdc@users.noreply.github.com>
2023-12-13 08:51:17 +0100

    Update Entrenador.java

--- Boletin15/Boletin15_1/Entrenador.java
+++ Boletin15/Boletin15_1/Entrenador.java
@@ -1,1 +1,47 @@
+package boletin15_1;
 
+public class Entrenador extends SeleccionFutbol{
+    
+    private String idFederacion;
+    
+    
+    
+    
+    public void dirixirPartido(){
+        
+    }
+    
+    public void dirixirEntrenamento(){
+        
+    }
+
+    public Entrenador() {
+    }
+
+    public Entrenador(String idFederacion) {
+        this.idFederacion = idFederacion;
+    }
+
+    public Entrenador(String idFederacion, int id, String nome, String apelidos, int edade) {
+        super(id, nome, apelidos, edade);
+        this.idFederacion = idFederacion;
+    }
+
+    
+ 
+    public String getIdFederacion() {
+        return idFederacion;
+    }
+
+    public void setIdFederacion(String idFederacion) {
+        this.idFederacion = idFederacion;
+    }
+
+    @Override
+    public String toString() {
+        return "Entrenador: "+super.toString() + ", idFederacion=" + idFederacion;
+    }
+    
+    
+    
+}
