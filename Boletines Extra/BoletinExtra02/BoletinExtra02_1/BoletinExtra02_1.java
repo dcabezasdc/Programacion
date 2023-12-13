ccc768c5f1a622e90cc1cea70d847567c08a429f
dcabezasdc <145324063+dcabezasdc@users.noreply.github.com>
2023-12-13 09:52:57 +0100

    Update BoletinExtra02_1.java

--- Boletines Extra/BoletinExtra02/BoletinExtra02_1/BoletinExtra02_1.java
+++ Boletines Extra/BoletinExtra02/BoletinExtra02_1/BoletinExtra02_1.java
@@ -1,1 +1,12 @@
+package boletinextra1_2;
 
+
+public class Boletinextra1_2 {
+
+  
+    public static void main(String[] args) {
+        System.out.println("Programacion" + "\nPrimer Parcial:"+8+"\nSegundo Parcial:"+7+"\nNota final:"+7.5);
+  
+    }
+    
+}
