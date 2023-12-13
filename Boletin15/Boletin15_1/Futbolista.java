8d67a566bf2536d3e8bc57728695d002731fec8e
dcabezasdc <145324063+dcabezasdc@users.noreply.github.com>
2023-12-13 08:52:02 +0100

    Update Futbolista.java

--- Boletin15/Boletin15_1/Futbolista.java
+++ Boletin15/Boletin15_1/Futbolista.java
@@ -1,1 +1,54 @@
+package boletin15_1;
 
+public class Futbolista extends SeleccionFutbol{
+    
+    private int dorsal;
+    private String demarcacion;
+    
+    public void xogarPartido(){
+        
+    }
+    
+    public void entrenar(){
+        
+    }
+
+    public Futbolista() {
+    }
+
+    public Futbolista(int dorsal, String demarcacion) {
+        this.dorsal = dorsal;
+        this.demarcacion = demarcacion;
+    }
+
+    public Futbolista(int dorsal, String demarcacion, int id, String nome, String apelidos, int edade) {
+        super(id, nome, apelidos, edade);
+        this.dorsal = dorsal;
+        this.demarcacion = demarcacion;
+    }
+
+    public int getDorsal() {
+        return dorsal;
+    }
+
+    public void setDorsal(int dorsal) {
+        this.dorsal = dorsal;
+    }
+
+    public String getDemarcacion() {
+        return demarcacion;
+    }
+
+    public void setDemarcacion(String demarcacion) {
+        this.demarcacion = demarcacion;
+    }
+
+    @Override
+    public String toString() {
+        return  "Futbolista: "+ super.toString()+", dorsal=" + dorsal + ", demarcacion=" + demarcacion ;
+    }
+    
+    
+    
+    
+}
