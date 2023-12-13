3536a9e3297899b8a709e3d0b84b1bdece627f34
dcabezasdc <145324063+dcabezasdc@users.noreply.github.com>
2023-12-13 09:04:00 +0100

    Update Libro.java

--- Boletin16/Boletin16_1/Libro.java
+++ Boletin16/Boletin16_1/Libro.java
@@ -1,1 +1,38 @@
+package boletin16_1;
 
+public class Libro extends Publicacion{
+    
+    private boolean prestado=false;
+
+    public Libro() {
+    }
+
+    public Libro(int codigo, String titulo, int anoPublicacion,boolean prestado) {
+        super(codigo, titulo, anoPublicacion);
+        this.prestado=prestado;
+    }
+
+    @Override
+    public int getCodigo() {
+        return super.getCodigo();
+    }
+    
+    @Override
+    public int getAnoPublicacion(){
+        return super.getAnoPublicacion();
+    }
+    
+    public boolean prestar(){
+        return prestado=true;
+    }
+    
+    @Override
+    public String toString() {
+        return "Libro: "+super.toString() + ", prestado=" + prestado;
+    }
+
+    
+    
+    
+    
+}
