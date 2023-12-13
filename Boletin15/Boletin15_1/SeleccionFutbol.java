2ba4a985513b2ecc232aef3694933bce85e5b3f8
dcabezasdc <145324063+dcabezasdc@users.noreply.github.com>
2023-12-13 08:56:25 +0100

    Update SeleccionFutbol.java

--- Boletin15/Boletin15_1/SeleccionFutbol.java
+++ Boletin15/Boletin15_1/SeleccionFutbol.java
@@ -1,1 +1,68 @@
+package boletin15_1;
 
+
+public class SeleccionFutbol {
+    
+    private int id;
+    private String nome;
+    private String apelidos;
+    private int edade;
+    
+    
+    public void concentrarse(){
+        
+    }
+    
+    public void viaxar(){
+        
+    }
+
+    public SeleccionFutbol() {
+    }
+
+    
+    
+    public SeleccionFutbol(int id, String nome, String apelidos, int edade) {
+        this.id = id;
+        this.nome = nome;
+        this.apelidos = apelidos;
+        this.edade = edade;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getApelidos() {
+        return apelidos;
+    }
+
+    public void setApelidos(String apelidos) {
+        this.apelidos = apelidos;
+    }
+
+    public int getEdade() {
+        return edade;
+    }
+
+    public void setEdade(int edade) {
+        this.edade = edade;
+    }
+
+    @Override
+    public String toString() {
+        return "id=" + id + ", nome=" + nome + ", apelidos=" + apelidos + ", edade=" + edade;
+    }
+}
