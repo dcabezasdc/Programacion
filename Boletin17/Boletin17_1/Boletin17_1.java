b565f793392b749a6f9b8f208157030453543186
dcabezasdc <145324063+dcabezasdc@users.noreply.github.com>
2023-12-13 09:17:53 +0100

    Update Boletin17_1.java

--- Boletin17/Boletin17_1/Boletin17_1.java
+++ Boletin17/Boletin17_1/Boletin17_1.java
@@ -1,1 +1,12 @@
+package boletin17_1;
 
+import javax.swing.JOptionPane;
+
+public class Boletin17_1 {
+
+    public static void main(String[] args) {
+        
+       Conta cont= new Conta();
+       cont.elexirConta();    
+    } 
+}
