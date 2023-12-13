b18146ab21953e5d391c19e98da007484cf37663
dcabezasdc <145324063+dcabezasdc@users.noreply.github.com>
2023-12-13 09:31:20 +0100

    Update Boletin18_1.java

--- Boletin18/Boletin18_1/Boletin18_1.java
+++ Boletin18/Boletin18_1/Boletin18_1.java
@@ -1,1 +1,16 @@
+package boletin18_1;
 
+
+public class Boletin18_1 {
+
+    public static void main(String[] args) {
+        
+        
+        Chamada cha= new Chamada();
+        cha.prezoChamada();
+       
+       
+        
+    }
+    
+}
