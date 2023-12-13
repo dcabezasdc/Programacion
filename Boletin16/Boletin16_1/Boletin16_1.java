f929784dcc1f658f1c633d2386a058ad6f4b2290
dcabezasdc <145324063+dcabezasdc@users.noreply.github.com>
2023-12-13 09:03:32 +0100

    Update Boletin16_1.java

--- Boletin16/Boletin16_1/Boletin16_1.java
+++ Boletin16/Boletin16_1/Boletin16_1.java
@@ -1,1 +1,14 @@
+package boletin16_1;
 
+public class Boletin16_1 {
+
+    public static void main(String[] args) {
+        
+        Revista rev=new Revista(5,5,"ppp",6);
+        System.out.println(rev);
+        
+        Libro lib =new Libro(8,"sdas",6,true);
+        
+        System.out.println(lib);
+    }    
+}
