bfa30296270c0fb95334c107c0b4c40e5ff1a18c
dcabezasdc <145324063+dcabezasdc@users.noreply.github.com>
2023-12-13 09:53:49 +0100

    Update BoletinExtra03_1.java

--- Boletines Extra/BoletinExtra03/BoletinExtra03_1/BoletinExtra03_1.java
+++ Boletines Extra/BoletinExtra03/BoletinExtra03_1/BoletinExtra03_1.java
@@ -1,1 +1,13 @@
+package boletinextra1_3;
 
+
+public class Boletinextra1_3 {
+
+    public static void main(String[] args) {
+        double prezo=90, rebaixa=0.75;
+        double prezof=prezo*rebaixa;
+        System.out.println("prezo final="+prezof+"€");
+        
+    }
+    
+}
