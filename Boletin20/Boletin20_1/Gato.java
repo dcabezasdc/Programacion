b2466bdc234d54be8b30ed3ed964d440b0a2cf83
dcabezasdc <145324063+dcabezasdc@users.noreply.github.com>
2024-03-06 11:47:07 +0100

    Create Gato.java

---
+++ Boletin20/Boletin20_1/Gato.java
@@ -0,0 +1,15 @@
+
+package boletin20_1;
+
+public class Gato extends Mamifero implements IPodeNadar{
+    
+    @Override
+    public void camiñar(){
+        
+    }
+    
+    @Override
+    public void nadar(){
+        
+    }    
+}
