93a193bfb60d1a0b6c3ab5d22881e90b7370d9df
dcabezasdc <145324063+dcabezasdc@users.noreply.github.com>
2024-03-06 11:51:31 +0100

    Create Aves.java

---
+++ Boletin20/Boletin20_2/Aves.java
@@ -0,0 +1,15 @@
+
+package boletin20_2;
+
+public class Aves implements IPodeVolar,IPodeCamiñar{
+    
+    @Override
+    public void volar(){
+        
+    }
+    
+    @Override
+    public void camiñar(){
+        
+    }
+}
