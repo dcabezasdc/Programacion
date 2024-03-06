9dae63f27ff487c3b64bb6df123470a38d376067
dcabezasdc <145324063+dcabezasdc@users.noreply.github.com>
2024-03-06 12:29:34 +0100

    Create Boletin21_1.java

---
+++ Boletin21/Boletin21_1/Boletin21_1.java
@@ -0,0 +1,13 @@
+
+package boletin21_1;
+
+public class Boletin21_1 {
+
+    public static void main(String[] args) {
+        
+        ArrayInverso obj =new ArrayInverso();
+        obj.crearArray();
+        
+    }
+    
+}
