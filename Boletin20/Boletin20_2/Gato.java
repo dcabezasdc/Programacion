0f1ede4b736683e41bd22114264df417f3eb419c
dcabezasdc <145324063+dcabezasdc@users.noreply.github.com>
2024-03-06 11:56:12 +0100

    Create Gato.java

---
+++ Boletin20/Boletin20_2/Gato.java
@@ -0,0 +1,15 @@
+
+package boletin20_2;
+
+public class Gato extends Felino{
+    
+    @Override
+    public void camiñar(){
+        
+    }
+    
+    @Override
+    public void nadar(){
+        
+    }    
+}
