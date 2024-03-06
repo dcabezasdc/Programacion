1cb38c386fbb588f83baa3b46fa75828aff1116f
dcabezasdc <145324063+dcabezasdc@users.noreply.github.com>
2024-03-06 12:00:41 +0100

    Create Papagaio.java

---
+++ Boletin20/Boletin20_2/Papagaio.java
@@ -0,0 +1,16 @@
+
+package boletin21_2;
+
+public class Papagaio extends Aves{
+    
+    @Override
+    public void camiñar(){
+        
+    }
+    
+    @Override
+    public void volar(){
+        
+    }
+    
+}
