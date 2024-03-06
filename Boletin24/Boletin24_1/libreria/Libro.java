9bcf209c1c3a6f766391f45d82d71cd07a6302fa
dcabezasdc <145324063+dcabezasdc@users.noreply.github.com>
2024-03-06 13:03:42 +0100

    Create Libro.java

---
+++ Boletin24/Boletin24_1/libreria/Libro.java
@@ -0,0 +1,58 @@
+
+package libreria;
+
+public class Libro {
+    private String titulo;
+    private String autor;
+    private String isbn;
+    private double precio;
+    private int unidades;
+
+    public Libro(String titulo, String autor, String isbn, double precio, int unidades) {
+        this.titulo = titulo;
+        this.autor = autor;
+        this.isbn = isbn;
+        this.precio = precio;
+        this.unidades = unidades;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+
+    public int getUnidades() {
+        return unidades;
+    }
+
+    public void setUnidades(int unidades) {
+        this.unidades = unidades;
+    }    
+}
