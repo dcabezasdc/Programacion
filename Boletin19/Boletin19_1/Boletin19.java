1c4b4ff94207709d5cbcae969e5f4efd9be9d60d
dcabezasdc <145324063+dcabezasdc@users.noreply.github.com>
2024-03-06 12:16:46 +0100

    Update Boletin19.java

--- Boletin19/Boletin19_1/Boletin19.java
+++ Boletin19/Boletin19_1/Boletin19.java
@@ -1,30 +1,15 @@
-package boletin19;
 
-import javax.swing.JOptionPane;
+package boletin19_1;
 
-public class Boletin19 {
+public class Boletin19_1 {
 
-       public static void main(String[] args) {
-        ConversorTemperaturas conversor = new ConversorTemperaturas();
-        boolean temp = false;
-
-        while (!temp) {
-            try {
-                float temperatura= Float.parseFloat(JOptionPane.showInputDialog("Introduzca la Temperatura"));
-
-                float fahrenheit = ConversorTemperaturas.centigradosAFharenheit(temperatura); // Corregido el nombre del método
-                System.out.println("Temperatura en Fahrenheit: " + fahrenheit);
-
-                float reamur = ConversorTemperaturas.centigradosAReamur(temperatura);
-                System.out.println("Temperatura en Reamur: " + reamur);
-                temp = true;
-            } catch (TemperaturaErradaExcepcion e) {
-                System.out.println(e.getMessage());
-            } catch (Exception e) {
-                System.out.println("Error inesperado: " + e.getMessage());
-
-            }
-        }
+    public static void main(String[] args) {
+        
+        Canario canario = new Canario();
+        Persoa tenor= new Persoa();
+        
+        tenor.cantar();
+        canario.cantar();
     }
     
 }
