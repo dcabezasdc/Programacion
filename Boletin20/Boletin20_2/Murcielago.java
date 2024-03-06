345d89feafc9eede018d4dc581aa91b664ecf5b9
dcabezasdc <145324063+dcabezasdc@users.noreply.github.com>
2024-03-06 12:00:15 +0100

    Create Murcielago.java

---
+++ Boletin20/Boletin20_2/Murcielago.java
@@ -0,0 +1,15 @@
+
+package boletin21_2;
+
+public class Murcielago extends Mamifero implements IPodeVolar{
+    
+    @Override
+    public void volar(){
+        
+    }
+    
+    @Override
+    public void camiñar(){
+        
+    }
+}
