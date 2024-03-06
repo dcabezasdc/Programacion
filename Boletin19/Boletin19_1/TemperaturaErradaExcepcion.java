dee308e7b9fa1b71f54d400a3a2f3b60b0e1aa88
dcabezasdc <145324063+dcabezasdc@users.noreply.github.com>
2024-03-06 09:53:01 +0100

    Update TemperaturaErradaExcepcion.java

--- Boletin19/Boletin19_1/TemperaturaErradaExcepcion.java
+++ Boletin19/Boletin19_1/TemperaturaErradaExcepcion.java
@@ -1,1 +1,11 @@
 
+package boletin19;
+
+public class TemperaturaErradaExcepcion extends Exception {
+    
+     public TemperaturaErradaExcepcion(String mensaje) {
+        super(mensaje);
+////            
+        }
+                
+    }
